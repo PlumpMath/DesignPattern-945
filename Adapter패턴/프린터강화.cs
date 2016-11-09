c8c5342b6e830a71ec9d50c5ac74585e82d9d032
didhddldlq <hi.high.grace@gmail.com>
2016-11-09 17:54:11 +0900

    굳

---
+++ Adapter패턴/프린터강화.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter패턴
+{
+    interface 프린터강화
+    {
+        string p태그로감싸기();
+        string span태그로감싸기();
+    }
+}
