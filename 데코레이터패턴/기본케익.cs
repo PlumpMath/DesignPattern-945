abc223cc707bf9312440b53d632e2220a266c124
didhddldlq <hi.high.grace@gmail.com>
2017-03-05 16:19:11 +0900

    키킼

---
+++ 데코레이터패턴/기본케익.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 데코레이터패턴
+{
+    class 기본케익 : 케익
+    {
+    }
+}
---
+++ 데코레이터패턴/케익재료/초콜렛.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 데코레이터패턴.케익재료
+{
+    class 초콜렛 : 케익데코
+    {
+    }
+}
